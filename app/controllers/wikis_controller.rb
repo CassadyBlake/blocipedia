b4d06d5df25661feae9b544588558c5279bfd98d
CassadyBlake <cassady.lillstrom@gmail.com>
2018-04-16 17:12:37 -0700

    user story 3 wiki CRUD: revision to add strong params and _form partial

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -13,9 +13,7 @@ class WikisController < ApplicationController
   end
 
   def create
-    @wiki = Wiki.new
-    @wiki.title = params[:wiki][:title]
-    @wiki.body = params[:wiki][:body]
+    @wiki = Wiki.new(wiki_params)
     @wiki.user = current_user
     if @wiki.save
       flash[:notice] = "Wiki was saved."
@@ -32,8 +30,14 @@ class WikisController < ApplicationController
 
   def update
     @wiki = Wiki.find(params[:id])
-    @wiki.title = params[:wiki][:title]
-    @wiki.body = params[:wiki][:body]
+    @wiki.assign_attributes(wiki_params)
+    if @wiki.save
+      flash[:notice] = "Wiki was updated."
+      redirect_to [@wiki]
+    else
+      flash.now[:alert] = "There was an error updating the wiki, please try again."
+      render :new
+    end
   end
 
   def destroy
@@ -46,6 +50,12 @@ class WikisController < ApplicationController
       flash.now[:alert] = "There was an error deleting the wiki."
       render :show
     end
+  end
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body)
   end
 
 end
